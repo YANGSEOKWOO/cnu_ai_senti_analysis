4eacfd6fca7f11728c6c94af485f75780cc89ba5
YANGSEOKWOO <59641097+YANGSEOKWOO@users.noreply.github.com>
2022-10-18 11:41:40 +0900

    Add files via upload

--- DaumNewsList.py
+++ DaumNewsList.py
@@ -1,13 +1,12 @@
 import pprint
-
 import requests
-
 from bs4 import BeautifulSoup
+from collector.CollectorService import get_daum_news
 
 url = 'https://news.daum.net/breakingnews/'
 
 result = requests.get(url)
-
+# SSL Error -> requests.get(url, verify=False)
 doc = BeautifulSoup(result.text, 'html.parser')  # result.text 파일을 html 형식의 파일로 바꾼다.print(doc)
 # <a href="url"> : a 태그는 클릭했 을 때 해당 url 로 이동 한다.
 # len(): list[]의 갯수를 알려 주는 함수
@@ -19,7 +18,8 @@ title_list = doc.select('ul.list_news2 a.link_txt')
 # list[]의 index 는 0번부터 시작
 # len(list) = 15, index = 0~14
 for i, title in enumerate(title_list):
-    print(f'인덱스:{i+1}, 제목: {title.get_text()}')
+    print(f'인덱스:{i+1}, 제목: {title["href"]}')
+    get_daum_news(title["href"])  # 뉴스 제목, 본문 수집 함수
 # href만 가져오고 싶다 -> title["href"]
 # text만 가져오고 싶다 -> title.get_text()
 
